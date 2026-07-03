8db446b9820c5f1251d8aea6d59a46fb61ea01fe
skies-starred <github@starred.foo>
2026-07-03 05:54:30 +0100

    fix(PercentSizeConstraint)

--- src/main/kotlin/xyz/aerii/athen/api/rendering/ui/dsl/constraints/impl/size/PercentSizeConstraint.kt
+++ src/main/kotlin/xyz/aerii/athen/api/rendering/ui/dsl/constraints/impl/size/PercentSizeConstraint.kt
@@ -5,10 +5,10 @@ import xyz.aerii.athen.api.rendering.ui.dsl.elements.primitives.base.impl.IPrimi
 
 class PercentSizeConstraint(val w: Float, val h: Float) : ISizeConstraint {
     override fun width(element: IPrimitiveElement<*>, parent: IPrimitiveElement<*>): Int {
-        return (parent.width * w).toInt()
+        return ((parent.width / 100f) * w).toInt()
     }
 
     override fun height(element: IPrimitiveElement<*>, parent: IPrimitiveElement<*>): Int {
-        return (parent.height * h).toInt()
+        return ((parent.height / 100f) * h).toInt()
     }
 }
